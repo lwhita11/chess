9748efe00f2abc1cc8cdc5caed75533e78746a96
Landon Whitaker <landonbballwhitaker@gmail.com>
2024-10-21 22:43:36 -0600

    finished getting started

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,15 @@
 import chess.*;
+import server.Server;
+import spark.Spark;
+import java.util.*;
+import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+        Server server = new Server();
+        server.run(8080);
     }
 }
+
