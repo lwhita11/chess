0782191d7dc37b2e8b3c44f87f9be42cc266794f
Landon Whitaker <landonbballwhitaker@gmail.com>
2024-11-03 16:26:44 -0700

    getUsername and deleteToken tests done

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -64,5 +64,40 @@ public class DataAccessTests {
         assertNotEquals(username, "test");
     }
 
+    @Test
+    void getUsername() throws ResponseException {
+        DATA_ACCESS.putToken("Username", "token");
+        String username = DATA_ACCESS.getUsername("token");
+        assertEquals(username, "Username");
+    }
+
+    @Test
+    void badGetUsername() throws ResponseException {
+        DATA_ACCESS.putToken("test", "Token");
+        String username = DATA_ACCESS.getUsername("NotaToken");
+        assertNull(username);
+    }
+
+    @Test
+    void deleteToken() throws ResponseException {
+        DATA_ACCESS.putToken("test", "Token");
+        String username = DATA_ACCESS.getUsername("Token");
+        assertEquals(username, "test");
+        DATA_ACCESS.deleteToken("Token");
+        String username2 = DATA_ACCESS.getUsername("Token");
+        assertNull(username2);
+    }
+
+    @Test
+    void badDeleteToken() throws ResponseException {
+        DATA_ACCESS.putToken("test", "Token");
+        String username = DATA_ACCESS.getUsername("Token");
+        assertEquals(username, "test");
+        DATA_ACCESS.deleteToken("NotAToken");
+        String username2 = DATA_ACCESS.getUsername("Token");
+        assertEquals(username2, username);
+    }
+
+
 
 }
