91091025cd265c8027259540a7aa3d7634653673
Landon Whitaker <landonbballwhitaker@gmail.com>
2024-12-09 13:16:19 -0700

    temporarily removed file to run code quality

--- client/src/main/java/server/NotificationHandler.java
+++ client/src/main/java/server/NotificationHandler.java
@@ -1,10 +1,10 @@
-package server;
-
-import websocket.messages.ServerMessage;
-
-public class NotificationHandler {
-
-    public interface NotificationHandler {
-        void notify(ServerMessage notification);
-    }
-}
+//package server;
+//
+//import websocket.messages.ServerMessage;
+//
+//public class NotificationHandler {
+//
+//    public interface NotificationHandler {
+//        void notify(ServerMessage notification);
+//    }
+//}
